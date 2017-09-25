89bf04e8bf12390014ff3dad07fa7bb960038b58
MacGyver1618 <macgyver1618@gmail.com>
2017-09-26 00:41:37 +0300

    Added event handling on socket

--- server.js
+++ server.js
@@ -1,13 +1,50 @@
-import express from 'express'
-import http from 'http'
-import socketio from 'socket.io'
+var app = require('express')()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+var _ = require('lodash')
 
-var app = express();
-var server = http.Server(app);
-var io = socketio(server);
+var users = {}
 
-server.listen(6680);
+server.listen(6680)
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/src/index.html');
-});
+  res.sendFile('index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log("connected on ", socket.id)
+  socket.on('message', (message) => {
+    console.log(message)
+  })
+  socket.on('disconnect', (socket) => {
+    console.log(socket.id, " disconnected")
+  })
+  socket.on('pong', (socket) => {
+    console.log("received ping from ", socket.id)
+  })
+  socket.on('join channel', (channel) => {
+    socket.join(channel)
+    console.log(socket.id, " joined channel ", channel)
+  })
+  socket.on('leave channel', (channel) => {
+    socket.leave(channel)
+  })
+  socket.on('room message', (message) => {
+    console.log("received room message to room ", message.room )
+    io.in(message.room).emit('room message', message)
+  })
+  socket.on('change name', (name) => {
+    // if exists then return error else change and store name
+    users[name] = socket.id
+    console.log(users)
+  })
+  socket.on('private message', (message) => {
+    // if user doesn't exist then error
+    console.log("reiceived private message ", message)
+    var target = users[message.user]
+    console.log("socket is ", target, " ", socket.to(target).id)
+    socket.to(target).emit('private message', message)
+  })
+})
+
+console.log("Listening on port 6680")
